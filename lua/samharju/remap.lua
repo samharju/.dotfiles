537ae453f5528470b6f822bce6947e0b3ecd2d03
Sami Harju <sami.harju@gmail.com>
2023-03-24 08:58:01 +0000

    remaps

--- lua/samharju/remap.lua
+++ lua/samharju/remap.lua
@@ -1,1 +1,18 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move block nicely
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m <-2<CR>>gv=gv")
+
+
+-- keep stuff centered
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+
+-- yank to system clipboard
+--vim.keymap.set("n", "<leader>y", "\"+y")
+--vim.keymap.set("v", "<leader>y", "\"+y")
+--vim.keymap.set("n", "<leader>Y", "\"+Y")
