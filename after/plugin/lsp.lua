e9f1b6bec5da3c691b425b129186ccca8f72f997
Sami Harju <sami.1.harju@nokia.com>
2023-03-30 08:08:12 +0000

    sync format on save

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -112,6 +112,6 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     callback = function()
-        vim.lsp.buf.format({ async = true })
+        vim.lsp.buf.format()
     end
 })
