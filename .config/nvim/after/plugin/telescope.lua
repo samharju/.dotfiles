d41dbb57963c38bb7decdbeb40a88393707de2ff
Sami Harju <sami.harju@gmail.com>
2023-04-11 08:22:43 +0000

    stufff

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -18,10 +18,3 @@ vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fe', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fs', function()
-    local input_string = vim.fn.input("Grep > ")
-    if input_string == '' then
-        input_string = vim.fn.expand('<cword>')
-    end
-    builtin.grep_string({ search = input_string })
-end)
