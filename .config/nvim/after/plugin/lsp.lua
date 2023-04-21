f56577512833ea6b913f9a0f85d87b45a8805ee1
Sami Harju <sami.harju@gmail.com>
2023-04-21 15:03:41 +0000

    move format to global mappings

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -98,9 +98,6 @@ local lsp_attach = function(client, bufnr)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<leader>fo', function()
-        vim.lsp.buf.format { async = true }
-    end, opts)
 end
 
 local lspconfig = require('lspconfig')
--- .config/nvim/lua/samharju/remap.lua
+++ .config/nvim/lua/samharju/remap.lua
@@ -26,6 +26,9 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>fo', function()
+    vim.lsp.buf.format { async = true }
+end)
 
 local grp = vim.api.nvim_create_augroup("Prewrites", {})
 
