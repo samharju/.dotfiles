119e0e6b2682749d4e0d0d39d9e46b11a91852c0
Sami Harju <sami.harju@gmail.com>
2023-04-26 19:27:19 +0000

    lualine conf

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,1 +1,36 @@
-require("lualine").setup()
+require('lualine').setup {
+    options = {
+        globalstatus = true,
+    },
+    tabline = {
+    },
+    winbar = {
+        lualine_y = { { 'filename', path = 1 } }
+    },
+    inactive_winbar = {
+        lualine_y = { { 'filename', path = 1 } }
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff' },
+        lualine_c = { { 'filename', path = 1 }, 'diagnostics' },
+        lualine_y = { { 'buffers', mode = 2 } },
+        lualine_x = { 'progress', 'filetype' },
+        lualine_z = { 'location' }
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+    },
+}
+
+vim.keymap.set("n", "<leader>j", function()
+    if vim.v.count == 0 then
+        return ":b#<CR>"
+    end
+    return "<cmd>LualineBuffersJump " .. vim.v.count .. "<CR>"
+end, { expr = true })
