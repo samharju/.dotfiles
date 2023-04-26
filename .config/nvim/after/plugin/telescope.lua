19a4c520f05aded31abe55b188a996fc31546409
Sami Harju <sami.harju@gmail.com>
2023-04-26 19:27:36 +0000

    telescope tuning

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,23 +1,24 @@
 require('telescope').setup {
+    defaults = {
+    },
     pickers = {
         find_files = {
-            theme = "dropdown",
+            theme = "ivy",
         },
         buffers = {
-            theme = "dropdown"
+            theme = "ivy"
         },
         current_buffer_fuzzy_find = {
-            theme = "dropdown"
+            theme = "ivy"
         }
     },
 }
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.current_buffer_fuzzy_find, {})
---
--- do not use gitignore
 vim.keymap.set('n', '<leader>fa', function() builtin.find_files({ no_ignore = true }) end, {})
-
 vim.keymap.set('n', '<leader>fe', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
