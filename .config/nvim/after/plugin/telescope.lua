89106200a34b41c4bde81c9e7f8f14bcc9dc0022
Sami Harju <sami.harju@gmail.com>
2023-05-05 18:43:54 +0300

    telescope find scopes

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -16,7 +16,8 @@ require('telescope').setup {
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.current_buffer_fuzzy_find, {})
-vim.keymap.set('n', '<leader>fa', function() builtin.find_files({ no_ignore = true }) end, {})
+vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fz', function() builtin.find_files({ no_ignore = true }) end, {})
 vim.keymap.set('n', '<leader>fe', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
