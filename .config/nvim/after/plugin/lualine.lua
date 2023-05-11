715f48007aeeb3dbc3a5236b6872887739cba609
Sami Harju <sami.harju@gmail.com>
2023-05-11 18:50:51 +0300

    lualine fiddling

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,31 +1,34 @@
 require('lualine').setup {
     options = {
         globalstatus = true,
+        refresh = {
+            statusline = 200,
+        }
     },
     tabline = {
     },
     winbar = {
         lualine_a = {},
-        lualine_b = { { 'filename', path = 1 } },
-        lualine_c = { 'diagnostics' },
+        lualine_b = {},
+        lualine_c = { { 'filename', path = 0 }, 'diagnostics' },
         lualine_x = {},
         lualine_y = {},
         lualine_z = {}
     },
     inactive_winbar = {
         lualine_a = {},
-        lualine_b = { { 'filename', path = 1 } },
-        lualine_c = { 'diagnostics' },
+        lualine_b = {},
+        lualine_c = { { 'filename', path = 0 }, 'diagnostics' },
         lualine_x = {},
         lualine_y = {},
         lualine_z = {}
     },
     sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch' },
-        lualine_c = { { 'filename', path = 1 }, 'diff', 'diagnostics' },
-        lualine_y = { { 'buffers', mode = 2 } },
+        lualine_b = { 'branch', { 'filename', path = 1 } },
+        lualine_c = { 'diff' },
         lualine_x = { 'progress', 'filetype' },
+        lualine_y = {},
         lualine_z = { 'location' }
     },
     inactive_sections = {
