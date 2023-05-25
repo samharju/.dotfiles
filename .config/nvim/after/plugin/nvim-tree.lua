ecdfee17388ebed66bef1bfaacb50ed695e57463
Sami Harju <sami.harju@gmail.com>
2023-05-25 11:13:37 +0300

    telescope looks updated

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -7,7 +7,6 @@ vim.opt.termguicolors = true
 
 -- empty setup using defaults
 local ntree = require("nvim-tree")
-local api = require("nvim-tree.api")
 
 
 ntree.setup {
@@ -34,17 +33,18 @@ ntree.setup {
 
 vim.keymap.set("n", "<leader>v", ":NvimTreeFindFile<CR>")
 
-local function open_nvim_tree(data)
-    -- open the tree if directory, change nvim root
-    local directory = vim.fn.isdirectory(data.file) == 1
-    if directory then
-        vim.cmd.cd(data.file)
-    end
+-- local api = require("nvim-tree.api")
+-- local function open_nvim_tree(data)
+--     -- open the tree if directory, change nvim root
+--     local directory = vim.fn.isdirectory(data.file) == 1
+--     if directory then
+--         vim.cmd.cd(data.file)
+--     end
+--
+--     -- open the tree if empty buffer
+--     if directory or (data.file == "" and vim.bo[data.buf].buftype == "") then
+--         api.tree.open({ focus = true, find_file = true })
+--     end
+-- end
 
-    -- open the tree if empty buffer
-    if directory or (data.file == "" and vim.bo[data.buf].buftype == "") then
-        api.tree.open({ focus = true, find_file = true })
-    end
-end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -11,7 +11,7 @@ require('telescope').setup {
             theme = "ivy",
         },
         buffers = {
-            theme = "ivy"
+            theme = "dropdown"
         },
         current_buffer_fuzzy_find = {
             theme = "ivy"
@@ -23,8 +23,24 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fz', function() builtin.find_files({ no_ignore = true }) end, {})
-vim.keymap.set('n', '<leader>fe', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fe', function() builtin.buffers ({ previewer = false }) end, {})
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+
+vim.api.nvim_create_augroup('startup', { clear = true })
+vim.api.nvim_create_autocmd('VimEnter', {
+    group = 'startup',
+    pattern = '*',
+    callback = function()
+        -- Open file browser if argument is a folder
+        local arg = vim.api.nvim_eval('argv(0)')
+        if arg and (vim.fn.isdirectory(arg) ~= 0 or arg == "") then
+            vim.defer_fn(function()
+                builtin.find_files({ previewer = false, layout_strategy = 'horizontal' })
+            end, 10)
+        end
+    end
+})
