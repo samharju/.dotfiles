6392323fcaf69c8c4c0bf15b1c02a18d6b99f3cc
Sami Harju <sami.harju@gmail.com>
2023-06-13 21:27:01 +0300

    chore: nvim tree as float

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -13,38 +13,46 @@ ntree.setup {
     view = {
         width = {},
         float = {
-            enable = true
+            enable = true,
+            quit_on_focus_loss = false,
+            open_win_config = {
+                relative = "cursor",
+                border = "rounded",
+                width = 30,
+                height = 30,
+                row = 1,
+                col = 1,
+            },
         }
+    },
+    filters = {
+        dotfiles = false,
     },
     renderer = {
         icons = {
-            git_placement = "after"
+            git_placement = "after",
+            modified_placement = "after"
         },
         highlight_git = true,
-        highlight_opened_files = "icon"
+        highlight_opened_files = "icon",
     },
     update_focused_file = {
         enable = true
     },
     diagnostics = {
         enable = true
+    },
+    modified = {
+        enable = true
+    },
+    actions = {
+        open_file = {
+            quit_on_open = true,
+            window_picker = {
+                chars = "AJKLSDF",
+            }
+        }
     }
 }
 
 vim.keymap.set("n", "<leader>v", ":NvimTreeFindFile<CR>")
-
--- local api = require("nvim-tree.api")
--- local function open_nvim_tree(data)
---     -- open the tree if directory, change nvim root
---     local directory = vim.fn.isdirectory(data.file) == 1
---     if directory then
---         vim.cmd.cd(data.file)
---     end
---
---     -- open the tree if empty buffer
---     if directory or (data.file == "" and vim.bo[data.buf].buftype == "") then
---         api.tree.open({ focus = true, find_file = true })
---     end
--- end
-
--- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
