32150bf5f548d22f0bdbabd44c16ca984186528b
Sami Harju <sami.harju@gmail.com>
2023-06-13 21:17:57 +0300

    style: ts st

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,11 +1,11 @@
 require("onedark").setup({
     style = 'deep',
     transparent = true,
-    toggle_style_key = "<leader>ts",
+    toggle_style_key = "<leader>st",
     toggle_style_list = { 'dark', 'darker', 'cool', 'deep' },
 })
 require('onedark').load()
 
 
 
-require('colorizer').setup()
+require('colorizer').setup{}
