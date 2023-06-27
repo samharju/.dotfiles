b01548aab5f9b666d83410706634801e4daa04bc
Sami Harju <sami.harju@gmail.com>
2023-06-27 14:23:27 +0300

    feat: harpoon tabline

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -1,3 +1,10 @@
+require("harpoon").setup({
+    tabline = true,
+    tabline_prefix = "   ",
+    tabline_suffix = "",
+})
+
+
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
@@ -9,3 +16,9 @@ vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
 vim.keymap.set("n", "<leader>t", ui.nav_next)
+
+vim.cmd('highlight! HarpoonInactive guibg=NONE guifg=#63698c')
+vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
+vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
+vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#63698c')
+vim.cmd('highlight! TabLineFill guibg=NONE guifg=white')
