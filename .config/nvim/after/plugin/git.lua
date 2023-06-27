e88cc107b31178efa296a851739644ee632b40dc
Sami Harju <sami.harju@gmail.com>
2023-06-27 19:56:11 +0300

    style: vertical git

--- .config/nvim/after/plugin/git.lua
+++ .config/nvim/after/plugin/git.lua
@@ -1,6 +1,8 @@
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gs", function()
+    vim.cmd("vert Git")
+end)
 
 vim.g.gitgutter_map_keys = 0
 vim.g.gitgutter_preview_win_floating = 0
