baf35ab27bd886f77e1acd534f304b18d553594a
Sami Harju <sami.harju@gmail.com>
2023-06-27 14:18:21 +0300

    refactor: move zenmode config

--- .config/nvim/after/plugin/other.lua
+++ .config/nvim/after/plugin/other.lua
@@ -3,5 +3,3 @@ vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
 vim.keymap.set("n", "<leader>gg", ":GitGutterLineHighlightsToggle<CR>")
-
-vim.keymap.set("n", "<leader>z", require("zen-mode").toggle)
--- .config/nvim/after/plugin/zenmode.lua
+++ .config/nvim/after/plugin/zenmode.lua
@@ -3,3 +3,5 @@ require("zen-mode").setup {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
 }
+
+vim.keymap.set("n", "<leader>z", require("zen-mode").toggle)
