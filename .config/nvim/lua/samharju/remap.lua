d6470ca8fbddecd08e3ae45305b68f8ec80112b1
Sami Harju <sami.harju@gmail.com>
2023-06-27 20:00:29 +0300

    fix: use backspace for diag

--- .config/nvim/lua/samharju/remap.lua
+++ .config/nvim/lua/samharju/remap.lua
@@ -24,10 +24,10 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<BS>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<BS>d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<BS>q', vim.diagnostic.setloclist)
 vim.keymap.set('n', '<leader><leader>', function()
     vim.lsp.buf.format { async = true }
 end)
