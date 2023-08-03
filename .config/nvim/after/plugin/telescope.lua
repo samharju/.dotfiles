65812c331e1a53446ebb6fc7e0c2b736560af247
Sami Harju <sami.harju@gmail.com>
2023-08-03 19:04:19 +0300

    feat: telescope updates

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -9,26 +9,35 @@ require('telescope').setup {
     pickers = {
         find_files = {
             theme = "ivy",
+            results_title = false,
+            previewer = false,
+            hidden = true,
         },
         buffers = {
-            theme = "dropdown"
+            theme = "dropdown",
+            previewer = false,
+            results_title = false
         },
         current_buffer_fuzzy_find = {
-            theme = "ivy"
+            theme = "ivy",
+            previewer = false,
+            results_title = false,
         }
     },
 }
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.current_buffer_fuzzy_find, {})
-vim.keymap.set('n', '<leader>fa', function() builtin.find_files({ hidden = true }) end, {})
-vim.keymap.set('n', '<leader>fz', function() builtin.find_files({ no_ignore = true, hidden = true }) end, {})
-vim.keymap.set('n', '<leader>fe', function() builtin.buffers({ previewer = false }) end, {})
-vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
-vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>ff', builtin.current_buffer_fuzzy_find, { desc = "tele current_buffer_fuzzy_find" })
+vim.keymap.set('n', '<leader>fa', builtin.find_files, { desc = "tele find_files" })
+vim.keymap.set('n', '<leader>fz', function() builtin.find_files({ no_ignore = true }) end,
+    { desc = "tele find_files no ignore" })
+vim.keymap.set('n', '<leader>fe', builtin.buffers, { desc = "tele buffers" })
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = "tele diagnostics" })
+vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "tele git_files" })
+vim.keymap.set('n', '<leader>fl', builtin.live_grep, { desc = "tele live_grep" })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "tele help_tags" })
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = "tele grep_string" })
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "tele keymaps" })
 
 vim.api.nvim_create_augroup('startup', { clear = true })
 vim.api.nvim_create_autocmd('VimEnter', {
@@ -40,7 +49,13 @@ vim.api.nvim_create_autocmd('VimEnter', {
         if arg and (vim.fn.isdirectory(arg) ~= 0 or arg == "") then
             local cwd = vim.fn.getcwd()
             vim.defer_fn(function()
-                builtin.find_files({ previewer = false, layout_strategy = 'center', prompt_title = cwd })
+                builtin.find_files({
+                    previewer = false,
+                    hidden = false,
+                    theme = 'vertical',
+                    layout_strategy = 'center',
+                    prompt_title = cwd
+                })
             end, 10)
         end
     end
--- .config/nvim/lua/samharju/packer.lua
+++ .config/nvim/lua/samharju/packer.lua
@@ -34,7 +34,7 @@ return require('packer').startup(function(use)
     -- movement
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
