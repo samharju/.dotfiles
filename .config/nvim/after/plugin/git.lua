99520ac5eda5ecd8ba66d6884d19c30c00c01701
Sami Harju <sami.harju@gmail.com>
2023-08-03 19:02:22 +0300

    fix: clean git keymaps

--- .config/nvim/after/plugin/git.lua
+++ .config/nvim/after/plugin/git.lua
@@ -1,15 +1,7 @@
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "UndotreeToggle" })
 
 vim.keymap.set("n", "<leader>gs", function()
     vim.cmd("vert Git")
-end)
+end, { desc = "git fugitive" })
 
-vim.g.gitgutter_map_keys = 0
-vim.g.gitgutter_preview_win_floating = 0
-
-vim.keymap.set("n", "<BS>g", ":GitGutterLineHighlightsToggle<CR>")
-vim.keymap.set("n", "<BS>d", ":GitGutterNextHunk<CR>")
-vim.keymap.set("n", "<BS>a", ":GitGutterPrevHunk<CR>")
-vim.keymap.set("n", "<BS>s", ":GitGutterStageHunk<CR>")
-vim.keymap.set("n", "<BS>x", ":GitGutterUndoHunk<CR>")
-vim.keymap.set("n", "<BS>w", ":GitGutterPreviewHunk<CR>")
+vim.keymap.set("n", "<leader>gg", ":GitGutterLineHighlightsToggle<CR>")
