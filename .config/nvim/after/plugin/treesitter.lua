a5a15a5315aa57711d8e8d6562ca3273bf4ae3c4
Sami Harju <sami.harju@gmail.com>
2023-08-03 19:06:11 +0300

    feat: treesitter selection fix

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "lua", "vim", "help", "query", "python", "go", "json" },
+    ensure_installed = { "c", "lua", "vim", "query", "python", "go", "json" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
@@ -24,8 +24,7 @@ require 'nvim-treesitter.configs'.setup {
         enable = true,
         keymaps = {
             init_selection = '<CR>',
-            scope_incremental = '<CR>',
-            node_incremental = '<TAB>',
+            node_incremental = '<CR>',
             node_decremental = '<S-TAB>',
         },
     },
