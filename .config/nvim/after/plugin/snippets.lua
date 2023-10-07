7f69999e01da00c03f15ba903d598e2b1b4b8573
Sami Harju <sami.harju@gmail.com>
2023-10-07 15:03:15 +0300

    feat: isots snippet

--- .config/nvim/after/plugin/snippets.lua
+++ .config/nvim/after/plugin/snippets.lua
@@ -1,1 +1,24 @@
+local ls = require('luasnip')
+
+local isots = function()
+    return { os.date("!%Y-%m-%dT%H:%M:%S.000Z") }
+end
+
+ls.add_snippets(nil, {
+    all = {
+        ls.snippet({
+                trig = "isots",
+                namr = "Iso date",
+                dscr = "Date in iso8601 format",
+            },
+            {
+                ls.function_node(isots, {})
+            })
+    }
+})
+
+
+
+
 require('luasnip.loaders.from_vscode').lazy_load()
+
