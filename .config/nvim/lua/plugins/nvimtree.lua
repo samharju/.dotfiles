9866f547df1fb7b6a75deca25b31da68abf3c731
Sami Harju <sami.harju@gmail.com>
2023-10-20 22:31:20 +0300

    refactor: fiddling with nvimtree

--- .config/nvim/lua/plugins/nvimtree.lua
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -1,5 +1,6 @@
 return {
     'nvim-tree/nvim-tree.lua',
+    lazy = false,
     dependencies = {
         'nvim-tree/nvim-web-devicons',
     },
@@ -7,18 +8,24 @@ return {
         { '<leader>v', ':NvimTreeFindFileToggle<CR>', desc = 'nvimtree toggle' }
     },
     opts = {
+        hijack_cursor = true,
         view = {
             float = {
                 enable = true,
                 quit_on_focus_loss = true,
-            }
+            },
+            signcolumn = "yes",
+            width = {} -- empty table means adaptive
         },
         filters = {
             git_ignored = false
         },
         renderer = {
             icons = {
-                git_placement = 'after',
+                git_placement = 'signcolumn',
+                glyphs = {
+                    git = { ignored = "" }
+                }
             },
             highlight_git = true,
         },
@@ -26,7 +33,8 @@ return {
             enable = true
         },
         diagnostics = {
-            enable = true
+            enable = true,
+            show_on_open_dirs = false
         },
         actions = {
             open_file = {
