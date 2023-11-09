1ba2d608344185991d663b00cf6e55924af78db9
Sami Harju <sami.harju@gmail.com>
2023-11-09 12:43:25 +0200

    refactor: sort opts

--- .config/nvim/lua/samharju/set.lua
+++ .config/nvim/lua/samharju/set.lua
@@ -1,42 +1,31 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
-vim.opt.swapfile = false
+vim.g.mapleader = " "
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.guicursor = "n-c-sm:block,v:hor20,i-ci-ve:ver25,r-cr-o:hor20-blinkon175-blinkoff150-blinkwait175"
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 12
-vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-
-vim.g.mapleader = " "
-
-vim.opt.cursorline = true
-
-vim.opt.list = true
 vim.opt.listchars:append "eol:↴"
--- vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "trail:⋅"
+vim.opt.list = true
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 12
+vim.opt.shiftwidth = 4
+vim.opt.signcolumn = "yes"
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+vim.opt.swapfile = false
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.updatetime = 50
+vim.opt.wrap = false
+vim.opt.clipboard = 'unnamedplus'
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.opt.foldenable = false
@@ -48,3 +37,10 @@ vim.diagnostic.config {
         source = 'always'
     }
 }
+
+
+-- vim.keymap.set({ "n", "v" }, "<leader>hh", function()
+--     local _, _, start, _ = vim.fn.getpos("'<")
+--     local _, _, stop, _ = vim.fn.getpos("'>")
+--     print(start .. stop)
+-- end)
