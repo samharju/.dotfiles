494a381b101f20339414aa9bb08c8a980c457b86
Sami Harju <sami.harju@gmail.com>
2023-11-24 16:18:34 +0200

    nvim: null-ls -> none-ls

--- .config/nvim/lua/plugins/nullls.lua
+++ .config/nvim/lua/plugins/nullls.lua
@@ -1,49 +1,31 @@
 return {
-    'jose-elias-alvarez/null-ls.nvim',
+    'nvimtools/none-ls.nvim',
     init = function()
         local null_ls = require('null-ls')
 
-        local function bootstrap()
-            -- only attach sources that are available
-            local sources = {
-                {
-                    source = null_ls.builtins.formatting.black,
-                    bin = 'black'
-                },
-                {
-                    source = null_ls.builtins.diagnostics.flake8,
-                    bin = 'flake8'
-                },
-                {
-                    source = null_ls.builtins.formatting.goimports,
-                    bin = 'goimports'
-                },
-                {
-                    source = null_ls.builtins.formatting.prettier,
-                    bin = 'prettier'
-                },
-                {
-                    source = null_ls.builtins.formatting.isort,
-                    bin = 'isort'
-                },
-                {
-                    source = null_ls.builtins.diagnostics.mypy,
-                    bin = 'mypy'
-                },
-            }
+        -- only attach sources that are available
+        local builtins = {
+            { source = null_ls.builtins.code_actions.gomodifytags, bin = 'gomodifytags' },
+            { source = null_ls.builtins.code_actions.impl,         bin = 'impl' },
+            { source = null_ls.builtins.diagnostics.flake8,        bin = 'flake8' },
+            { source = null_ls.builtins.diagnostics.mypy,          bin = 'mypy' },
+            { source = null_ls.builtins.formatting.black,          bin = 'black' },
+            { source = null_ls.builtins.formatting.gofmt,          bin = 'gofmt' },
+            { source = null_ls.builtins.formatting.goimports,      bin = 'goimports' },
+            { source = null_ls.builtins.formatting.golines,        bin = 'golines' },
+            { source = null_ls.builtins.formatting.isort,          bin = 'isort' },
+            { source = null_ls.builtins.formatting.prettier,       bin = 'prettier' },
+        }
 
-            M = {}
-            for _, v in ipairs(sources) do
-                if vim.fn.executable(v.bin) == 1 then
-                    table.insert(M, v.source)
-                end
+        local sources = {}
+        for _, v in ipairs(builtins) do
+            if vim.fn.executable(v.bin) == 1 then
+                table.insert(sources, v.source)
             end
-            return M
         end
 
         null_ls.setup({
-            sources = bootstrap()
+            sources = sources
         })
     end
-
 }
