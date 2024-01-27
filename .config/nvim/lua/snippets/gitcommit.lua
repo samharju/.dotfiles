4278de18d8e7e5024e68faa06d0e1ee1e2dd2aa1
Sami Harju <sami.harju@gmail.com>
2024-01-27 19:08:18 +0200

    snippets: gitcommit

---
+++ .config/nvim/lua/snippets/gitcommit.lua
@@ -0,0 +1,31 @@
+---@diagnostic disable: undefined-global
+return {
+
+    s(
+        {
+            trig = 'cc',
+            name = 'conventional commit',
+            snippetType = 'autosnippet'
+        },
+        {
+            c(1, {
+                t('build'),
+                t('chore'),
+                t('ci'),
+                t('docs'),
+                t('feat'),
+                t('fix'),
+                t('perf'),
+                t('refactor'),
+                t('revert'),
+                t('style'),
+                t('test'),
+            }),
+            i(2, '(scope)'),
+            t(': '),
+            i(3, 'summary'),
+            t({ '', '', '' }),
+            i(4, 'body'),
+        }
+    ),
+}
