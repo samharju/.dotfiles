ae27bfd399ed5e5d43705989162e836dc79ac035
Sami Harju <sami.harju@gmail.com>
2024-02-03 17:11:34 +0000

    nvim: harpoon -> harpoon2

---
+++ .config/nvim/lua/plugins/harpoon2.lua
@@ -0,0 +1,23 @@
+return {
+    'theprimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+    },
+    config = function()
+        local harpoon = require('harpoon')
+        harpoon:setup({
+        })
+
+
+        vim.keymap.set('n', '<leader>a', function() harpoon:list():append() end)
+        vim.keymap.set('n', '<leader>e', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+        vim.keymap.set('n', '<leader>1', function() harpoon:list():select(1) end, { desc = 'harpoonfile 1' })
+        vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end, { desc = 'harpoonfile 2' })
+        vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end, { desc = 'harpoonfile 3' })
+        vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end, { desc = 'harpoonfile 4' })
+        vim.keymap.set('n', '<leader>n', function() harpoon:list():next() end, { desc = 'harpoon next' })
+    end
+
+}
