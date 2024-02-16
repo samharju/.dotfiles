6a691996defb69b38730fc57c9d98bbd77a89417
Sami Harju <sami.harju@gmail.com>
2024-02-16 22:52:17 +0200

    nvim: telescope filename first

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,23 @@ return {
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = function()
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "TelescopeResults",
+            callback = function(ctx)
+                vim.api.nvim_buf_call(ctx.buf, function()
+                    vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+                    vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+                end)
+            end,
+        })
+
+        local function filenameFirst(_, path)
+            local tail = vim.fs.basename(path)
+            local parent = vim.fs.dirname(path)
+            if parent == "." then return tail end
+            return string.format("%s\t\t%s", tail, parent)
+        end
+
         require("telescope").setup({
             defaults = {
                 file_ignore_patterns = { "%.git/" },
@@ -20,6 +37,7 @@ return {
                     theme = "dropdown",
                     results_title = false,
                     previewer = false,
+                    path_display = filenameFirst,
                 },
                 buffers = {
                     theme = "dropdown",
