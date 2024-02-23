ba5e2097fe95b2c906dc753039cd7eb24cb762b7
Sami Harju <sami.harju@gmail.com>
2024-02-23 13:06:14 +0200

    nvim: yeet and synthweave

--- .config/nvim/lua/plugins/synthweave.lua
+++ .config/nvim/lua/plugins/synthweave.lua
@@ -1,5 +1,5 @@
 return {
-    dir = "~/plugins/samharju/synthweave.nvim",
+    "samharju/synthweave.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000,
     config = function() vim.cmd.colorscheme("synthweave") end,
