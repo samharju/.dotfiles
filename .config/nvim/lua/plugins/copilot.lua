51d6a0dc0769ed0571ef564040cb12d71b3d5f8c
Sami Harju <sami.harju@gmail.com>
2024-04-15 21:12:39 +0300

    nvim: copilot settings

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -2,8 +2,25 @@ return {
     {
         "gptlang/CopilotChat.nvim",
         enabled = os.getenv("USE_COPILOT") == "true",
-        after = {
+        dependecies = {
             "zbirenbaum/copilot.lua",
+        },
+        build = function()
+            vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+        end,
+        opts = {
+            show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
+            debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
+            disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
+            language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
+            -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
+            -- temperature = 0.1,
+            prompts = {
+                Explain = "Explain this code to me very briefly.",
+                Review = "Review the following code and provide brief suggestions.",
+                Tests = "Generate unit tests for this code.",
+                Refactor = "Refactor the code to improve clarity and readability.",
+            },
         },
     },
     {
@@ -13,14 +30,14 @@ return {
         cmd = "Copilot",
         opts = {
             panel = {
-                enabled = false,
-                auto_refresh = false,
+                enabled = true,
+                auto_refresh = true,
                 keymap = {
-                    jump_prev = "<M-[>",
-                    jump_next = "<M-]>",
-                    accept = "<M-\\>",
-                    refresh = "<M-=>",
-                    open = "<M-BS>",
+                    jump_prev = "[[",
+                    jump_next = "]]",
+                    accept = "<CR>",
+                    refresh = "<M-]>",
+                    open = "<M-p>",
                 },
                 layout = {
                     position = "right", -- | top | left | right
@@ -41,7 +58,7 @@ return {
                 },
             },
             filetypes = {
-                yaml = false,
+                yaml = true,
                 markdown = false,
                 help = false,
                 gitcommit = false,
