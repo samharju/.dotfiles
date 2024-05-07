039e9e4d53f774dd812c4b5b6bfa2d8d192f3db0
Sami Harju <sami.harju@gmail.com>
2024-05-08 00:06:57 +0300

    nvim: lazy ignores changes

--- .config/nvim/lua/samharju/lazy.lua
+++ .config/nvim/lua/samharju/lazy.lua
@@ -14,4 +14,5 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins", {
     defaults = { version = "*" },
     dev = { path = "~/plugins/samharju/" },
+    change_detection = { enabled = false },
 })
