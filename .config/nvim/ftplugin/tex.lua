d260ddf084d13250304c5089ae650ed19ae78dae
Sami Harju <sami.harju@gmail.com>
2024-06-08 17:42:26 +0300

    nvim: tex options

--- .config/nvim/ftplugin/tex.lua
+++ .config/nvim/ftplugin/tex.lua
@@ -4,5 +4,5 @@ vim.opt.textwidth = tw
 vim.opt.linebreak = true
 vim.opt.wrap = true
 vim.opt.colorcolumn = { tw + 1 }
-vim.opt.formatoptions = "tcqa"
-vim.opt.spell = true
+vim.opt.formatoptions = "tqa"
+vim.opt.spell = false
