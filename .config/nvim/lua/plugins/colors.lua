3813497faa142fdad76f826de519f5375b1c6dfb
Sami Harju <sami.harju@gmail.com>
2024-08-28 09:46:41 +0300

    nvim: cycle colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,3 +1,21 @@
+local colors = {
+    "serene",
+    "serene-transparent",
+    "synthweave",
+    "synthweave-transparent",
+    "rose-pine-main",
+    "rose-pine-moon",
+    "eldritch",
+}
+
+local i = 1
+
+vim.keymap.set("n", "<leader>cn", function()
+    if i == #colors then i = 0 end
+    i = i + 1
+    vim.cmd.colorscheme(colors[i])
+end)
+
 return {
     {
         "eldritch-theme/eldritch.nvim",
