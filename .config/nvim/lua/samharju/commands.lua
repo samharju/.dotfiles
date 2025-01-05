5c3ac91ef52cc3fb24cb74ad111def9402f303f5
Sami Harju <sami.harju@gmail.com>
2025-01-05 23:55:15 +0200

    nvim: autocommand for undercurlies

--- .config/nvim/lua/samharju/commands.lua
+++ .config/nvim/lua/samharju/commands.lua
@@ -8,34 +8,55 @@ cmd("Breakpoints", function()
     vim.cmd([[ copen ]])
 end, {})
 
--- clear registers
-cmd("ClearRegs", function(_)
-    local regs = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    for r in regs:gmatch(".") do
-        local val = vim.fn.getreg(r)
-        if val ~= "" then vim.fn.setreg(r, "") end
+cmd("ToggleDiagnostics", function(args)
+    local conf = vim.diagnostic.config()
+    if conf == nil then return end
+    local opts = {
+        all = function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end,
+        signs = function() vim.diagnostic.config({ signs = not conf.signs }) end,
+        virtual_text = function()
+            vim.diagnostic.config({
+                virtual_text = not conf.virtual_text,
+            })
+        end,
+    }
+    for _, c in ipairs(args.fargs) do
+        opts[c]()
     end
-end, { desc = "Clear registers abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" })
-
-cmd(
-    "DiagnosticToggle",
-    function(_) vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end,
-    { desc = "Toggle diagnostics" }
-)
+end, { desc = "Toggle diagnostics", nargs = "+", complete = function() return { "all", "virtual_text" } end })
 
 local grp = vim.api.nvim_create_augroup("sharju_commands", { clear = true })
+
+local fg_from = function(group)
+    local hl = vim.api.nvim_get_hl(0, { name = group })
+    return hl.fg
+end
 
 vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
     group = grp,
     pattern = "*",
     callback = function()
-        vim.fn.matchadd("Function", [[TODO\|FIXME\|\<FIX\>]], 100)
+        vim.fn.matchadd("Todo", [[TODO\|FIXME\|\<FIX\>]], 100)
 
-        vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { italic = true })
-        vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { italic = true })
-        vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { italic = true })
-        vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { italic = true })
-        vim.api.nvim_set_hl(0, "DiagnosticUnderlineOk", { italic = true })
+        vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = fg_from("DiagnosticError") })
+        vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true, sp = fg_from("DiagnosticWarn") })
+        vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { undercurl = true, sp = fg_from("DiagnosticInfo") })
+        vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { undercurl = true, sp = fg_from("DiagnosticHint") })
+        vim.api.nvim_set_hl(0, "DiagnosticUnderlineOk", { undercurl = true, sp = fg_from("DiagnosticOk") })
+
+        vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { link = "Keyword", default = true })
+
+        vim.api.nvim_set_hl(0, "CmpItemKindClass", { link = "Type", default = true })
+        vim.api.nvim_set_hl(0, "CmpItemKindMethod", { link = "Function", default = true })
+        vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { link = "Keyword", default = true })
+        vim.api.nvim_set_hl(0, "CmpItemKindConstant", { link = "Constant", default = true })
+        vim.api.nvim_set_hl(0, "CmpItemKindFunction", { link = "Function", default = true })
+        vim.api.nvim_set_hl(0, "CmpItemKindOperator", { link = "Operator", default = true })
+        vim.api.nvim_set_hl(0, "CmpItemKindVariable", { link = "Variable", default = true })
+        vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { link = "TypeParameter", default = true })
+
+        vim.api.nvim_set_hl(0, "fugitiveUnstagedModifier", { link = "Removed", default = true })
+        vim.api.nvim_set_hl(0, "fugitiveStagedModifier", { link = "Added", default = true })
 
         vim.cmd([[hi! link @custom_injection ColorColumn]])
     end,
