a4464a19ce8c0a4928801de47d8529bcfbeb0c2d
Sami Harju <sami.harju@gmail.com>
2025-02-22 15:08:40 +0200

    nvim: fix compose/ansible ft detection

--- .config/nvim/after/ftplugin/yaml.lua
+++ .config/nvim/after/ftplugin/yaml.lua
@@ -1,5 +1,15 @@
-if string.match(vim.fn.expand("%:t"), "compose") then vim.bo.filetype = "yaml.docker-compose" end
+if string.match(vim.fn.expand("%:t"), "docker%-compose") then vim.bo.filetype = "yaml.docker-compose" end
 
-if string.match(vim.fn.expand("%:p"), "ansible") then vim.bo.filetype = "yaml.ansible" end
+if string.match(vim.fn.expand("%:h"), "ansible") then
+    vim.bo.filetype = "yaml.ansible"
+else
+    local content = vim.api.nvim_buf_get_lines(0, 0, 30, false) or { "" }
+    for _, value in ipairs(content) do
+        if string.find(value, "ansible.builtin") then
+            vim.bo.filetype = "yaml.ansible"
+            break
+        end
+    end
+end
 
 vim.opt_local.listchars = { leadmultispace = "▏ " }
