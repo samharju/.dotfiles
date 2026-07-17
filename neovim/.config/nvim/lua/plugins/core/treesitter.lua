7bff637bb7c38118d26d574645d2c021697f3ff3
Sami Harju <sami.harju@gmail.com>
2026-07-17 21:35:21 +0300

    nvim: treesitter to main

--- neovim/.config/nvim/lua/plugins/core/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/core/treesitter.lua
@@ -5,33 +5,44 @@ local is_big = function(_, bufnr)
 
     return s.size > 1000000
 end
-
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function(ev)
+        local lang = vim.treesitter.language.get_lang(ev.match)
+        local available_langs = require("nvim-treesitter").get_available()
+        local is_available = vim.tbl_contains(available_langs, lang)
+        if is_available then
+            local installed_langs = require("nvim-treesitter").get_installed()
+            local installed = vim.tbl_contains(installed_langs, lang)
+            if not installed then require("nvim-treesitter").install(lang):wait() end
+            vim.treesitter.start()
+            require("nvim-treesitter").indentexpr()
+        end
+    end,
+})
 return {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-        "windwp/nvim-ts-autotag",
-    },
-    version = false,
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-                disable = is_big,
-            },
-            autotag = {
-                enable = true,
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<leader><CR>",
-                    node_incremental = "<CR>",
-                    node_decremental = "<BS>",
-                },
-            },
-        })
-    end,
+    -- config = function()
+    --     require("nvim-treesitter.configs").setup({
+    --         auto_install = true,
+    --         highlight = {
+    --             enable = true,
+    --             additional_vim_regex_highlighting = false,
+    --             disable = is_big,
+    --         },
+    --         autotag = {
+    --             enable = true,
+    --         },
+    --         incremental_selection = {
+    --             enable = true,
+    --             keymaps = {
+    --                 init_selection = "<leader><CR>",
+    --                 node_incremental = "<CR>",
+    --                 node_decremental = "<BS>",
+    --             },
+    --         },
+    --     })
+    -- end,
 }
